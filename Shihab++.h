4448f88d1bec1eb5910fb080ebb6eebe633fa8a4
shihab <shihabshahriar543@gmail.com>
2024-09-28 01:53:16 +0600

    v1.0 Shihab++.h

---
+++ Shihab++.h
@@ -0,0 +1,76 @@
+// My own bangla funny language for C++
+#pragma once
+
+//libraries
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+//keywords
+#define ashol_khela_shuru int main()   // main function
+#define khela_sesh return 0           // end program
+#define jodi if                       // if statement
+#define nahoy else                    //else statement
+#define jotokkhon while               // while loop
+#define thamish_na do                // do statement
+#define kono_ak_karone for            // for loop
+#define shotti true                   // true boolean
+#define mittha false                  // false boolean
+#define faka void                     // void keyword
+#define kisu_de return                // return from function
+#define r_a_bondo_kor break           // break statement
+#define chalay_ja continue            // continue statement
+#define palta switch                  // switch statement
+#define hoyto case                    // case statement
+#define jonmo_theke default          // default keyword
+#define chesta_kor try               // try statement
+#define thela_shamla catch           // catch statement
+#define mar_dhil throw               // throw statement
+#define dhil_marish_na noexcept      // noexcept keyword
+#define jukto_kor include           // includes keyword for headers
+
+
+//data types
+#define gota_sonkha int     //integer
+#define soto_doshomic float  //float
+#define boro_doshomic double //double
+#define okkhor char          // character
+#define boltesi string       // string
+#define shotti_mittha bool  // boolean
+#define kisu_na nullptr    // nullptr
+#define laga_vector vector // vector
+#define laga_list list    // list
+#define laga_map map     // map
+#define laga_set set    // set
+
+// modifiers and operators
+#define a_rakh =      //assiggnment
+#define soman ==      // equality operator
+#define oshoman !=    // inequality operator
+#define soto <       // less than operator
+#define boro > // greater than operator
+#define soto_abar_soman <=  //less than equal
+#define boro_abar_soman >= //greater than equal
+#define ebong  &&   // logical AND 
+#define othoba || // logical OR
+#define nai !   // logical NOT
+#define barao ++ // increment
+#define komao -- // decrement
+#define jog +   // plus
+#define biyog - // minus
+#define gunn *  // multiply
+#define vaag   / // divission
+#define vaag_sesh %  //modulus
+
+// for i/o
+#define dekhi cout <<    // output
+#define assa_ne cin >>   // input
+#define notun_line  endl // newline
+
+//others 
+#define lekha_shuru { //opening brace
+#define sesh }        // closing brace
